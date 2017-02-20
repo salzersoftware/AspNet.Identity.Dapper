fecf86f69b1be6bf1c0bbe29fed48cc92d584e7e
Matthew Salzer <msalzer@salzersoftware.com>
2017-02-19 23:37:47 -0600

    - Folder Organization - Added Connection classes.

---
+++ AspNet.Identity.Dapper/Connection/Interfaces/IDbConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ConsumerApp.Database.Connections.Interfaces
+{
+    public interface IDbConnectionFactory
+    {
+        Task<SqlConnection> GetOpenConnectionAsync();
+    }
+}
---
+++ AspNet.Identity.Dapper/Connection/SqlConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using ConsumerApp.Database.Connections.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerApp.Database.Connections
+{
+    public class SqlConnectionFactory : IDbConnectionFactory
+    {
+        private string ConnectionString { get; set; }
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public async Task<SqlConnection> GetOpenConnectionAsync()
+        {
+            var connection = new SqlConnection(ConnectionString);
+
+            await connection.OpenAsync();
+
+            return connection;
+        }
+    }
+}
